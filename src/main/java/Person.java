9fd166e485c7bb8c14589e80c25f51e01b97cff1
igoreta <ivoronline@gmail.com>
2021-05-03 12:16:02 +0200

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,10 @@
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Person {
+  public Integer id;
+  public String  name;
+  public Integer age;
+}
+
+
---
+++ src/main/java/Test.java
@@ -0,0 +1,16 @@
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Test {
+
+  public static void main(String[] args) throws JAXBException, FileNotFoundException {
+    Unmarshaller unmarshaller = JAXBContext.newInstance(Person.class).createUnmarshaller();
+    Person       person       = (Person) unmarshaller.unmarshal(new FileReader("person.xml"));
+    System.out.println(person.name + " is " + person.age + " years old");
+  }
+
+
+}
